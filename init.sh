814135064aa7ceb2d183777495f6ecac0d2abddf
Mobodot <mobosomto@gmail.com>
2022-09-15 17:25:09 +0100

    base skeleton

---
+++ init.sh
@@ -0,0 +1,9 @@
+echo [$(date)]: "START"
+export _VERSION_ = 3.8
+echo [$(date)]: "Creating environment with python version: ${_VERSION_}"
+conda create --prefix ./env python=$(_VERSION_) -y
+echo [$(date)]: "Activating environment"
+source activate ./env
+echo [$(date)]: "Installing the dev requirements"
+pip install -r requirements_dev.txt
+echo [$(date)]: "END"
---
+++ template.py
@@ -0,0 +1,59 @@
+import os
+import logging
+from pathlib import Path
+
+
+logging.basicConfig(
+     level=logging.INFO,
+     format="[%(asctime)s: %(levelname)s:] %(message)s"
+)
+
+
+while True:
+    project_name = input("Enter project name: ")
+    if project_name != "":
+        break
+
+
+logging.info(f"Creating project by name: {project_name}")
+
+
+LIST_OF_PATHS = [
+    ".github/workflows/.gitkeep",
+    "configs/config.yaml",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    "tests/__init__.py",
+    "tests/integration/__init__.py",
+    "tests/unit/__init__.py",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.cfg",
+    "setup.py",
+    "tox.ini",
+    "pyproject.toml",
+    "init.sh"
+]
+
+
+for filepath in LIST_OF_PATHS:
+    filepath = Path(filepath)
+    file_dir, file_name = os.path.split(filepath)
+    if file_dir != "":
+        logging.info(f"Creating directory: {file_dir} for file: {file_name}")
+        os.makedirs(name=file_dir, exist_ok=True)
+        
+    if (not os.path.exists(filepath) or os.path.getsize(filepath) == 0):
+        with open(Path(filepath), "w") as f:
+            pass # create an empty 
+            logging.info(f"Creating an empty file: {file_name} in: {filepath}")
+            
+    else:
+        logging.info(f"File is already present at {filepath}")
+    
+    
