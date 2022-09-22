5955d2e014d302e0062b98a2b32b0ceaf2a5399d
Mobodot <mobosomto@gmail.com>
2022-09-22 09:32:52 +0100

    PrepareBaseModel Config

--- src/deepClassifier/entity/config_entity.py
+++ src/deepClassifier/entity/config_entity.py
@@ -3,9 +3,21 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
-@dataclass
+@dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
     source_url: Path
-    local_data_filei: Path
+    local_data_file: Path
     unzipped_dir: Path
+    
+    
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+    params_learning_rate: float
