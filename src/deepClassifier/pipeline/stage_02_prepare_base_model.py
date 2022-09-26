1bf8380d54d4a4240a2cb1f4adb6f3ef1312edf8
Mobodot <mobosomto@gmail.com>
2022-09-26 22:21:09 +0100

    prepare base model runner  func

--- src/deepClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/deepClassifier/pipeline/stage_02_prepare_base_model.py
@@ -8,22 +8,24 @@ STAGE_NAME = "Prepare Base Model Stage"
 def main():
 
     config = ConfigurationManager()
-    prepare_base_model_config = config.get_prepare_base_model()
+    prepare_base_model_config = config.get_prepare_base_model_config()
     prepare_base_model = PrepareBaseModel(
         prepare_base_model_config=prepare_base_model_config
         )
-    # prepare_base_model.get_base_model()
+    prepare_base_model.get_base_model()
     prepare_base_model.update_base_model()
     
 
 if __name__ == "__main__":
     try:
         print("\n")
+        logger.info("\n")
         logger.info(">"*30)
-        logger.info("Prepare Base Model Stage Started")
+        logger.info(f"{STAGE_NAME} Started")
         main()
-        logger.info("Prepare Base Model Stage completed")
+        logger.info(f"{STAGE_NAME} completed")
         logger.info("*"*30)
+        
         print("\n")
     except Exception as e:
         raise e
