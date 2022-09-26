4242c072b236973e3ec25888c2aeff82fe69eee6
Mobodot <mobosomto@gmail.com>
2022-09-26 22:16:59 +0100

    callbacks and training config

--- src/deepClassifier/config/configuration.py
+++ src/deepClassifier/config/configuration.py
@@ -1,11 +1,14 @@
 
 from deepClassifier import logger
 from pathlib import Path
+import os
 from deepClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from deepClassifier.utils import read_yaml, create_directories
-from deepClassifier.entity.config_entity import (
+from deepClassifier.entity import (
     DataIngestionConfig, 
-    PrepareBaseModelConfig
+    PrepareBaseModelConfig,
+    PrepareCallbacksConfig,
+    TrainingConfig
 )
 
 class ConfigurationManager:
@@ -33,7 +36,7 @@ class ConfigurationManager:
         return data_ingestion_config
         
         
-    def get_prepare_base_model(self) -> PrepareBaseModelConfig:
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
         
         create_directories([config.root_dir])
@@ -49,3 +52,38 @@ class ConfigurationManager:
             params_learning_rate=self.params.LEARNING_RATE
         )
         
+    def get_prepare_callbacks_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([config.tensorboard_root_log_dir,
+                            model_ckpt_dir])
+        
+        return PrepareCallbacksConfig(
+            root_dir=config.root_dir,
+            tensorboard_root_log_dir=config.tensorboard_root_log_dir,
+            checkpoint_model_filepath=config.checkpoint_model_filepath
+        )
+        
+    def get_training_config(self) -> TrainingConfig:
+        config = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        training_data = os.path.join(
+            self.config.data_ingestion.unzipped_dir, "PetImages")
+        
+        create_directories([config.root_dir])
+        
+        return TrainingConfig(
+            root_dir=config.root_dir,
+            trained_model_path=config.trained_model_path,
+            updated_base_model_path=prepare_base_model.updated_base_model_path,
+            training_data = training_data,
+            params_epochs=self.params.EPOCHS,
+            params_batch_size=self.params.BATCH_SIZE,
+            params_is_augmentation=self.params.AUGUMENTATION,
+            params_image_size=self.params.IMAGE_SIZE
+        )
+        
+config = ConfigurationManager()
+training_config = config.get_training_config()
+print(training_config)
